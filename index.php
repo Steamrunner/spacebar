c815839913b2e10efe4c6606638525173d2ea007
bloemist <bloemistwashere@gmail.com>
2016-10-23 19:34:33 +0200

    added reload button

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
 						<div class="col-sm-12">
 							<a class="btn btn-lg btn-filled btn-grey" onClick="ViewSection('page_config_products')">PRODUCTS</a>
 							<a class="btn btn-lg btn-filled btn-grey" onClick="ViewSection('page_add_account')">ACCOUNTS</a>
-							<a class="btn btn-lg btn-filled btn-grey" onClick="Reload()">reload</a>
+							<a class="btn btn-lg btn-filled" onClick="Reload()">reload</a>
 							<a class="btn btn-lg btn-filled btn-red" onClick="abort()">abort</a>
 						</div>
 				</div>
