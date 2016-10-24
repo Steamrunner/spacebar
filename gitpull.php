3a01417ff500cd4ac4835edd003a7741da56f63c
bloemist <bloemistwashere@gmail.com>
2016-10-24 20:20:21 +0200

    fixing link

--- gitpull.php
+++ gitpull.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'includes/settings.php';
+include_once 'includes/config.php';
 if(!$config['demo']){
 $output = shell_exec('git pull 2>&1');
 echo $output;
--- terminalpush.php
+++ terminalpush.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'includes/settings.php';
+include_once 'includes/config.php';
 if(!$config['demo']){
 $output = shell_exec('printf "\x0C'.$_GET['msg'].'" > /dev/ttyS0');
 echo $output;
